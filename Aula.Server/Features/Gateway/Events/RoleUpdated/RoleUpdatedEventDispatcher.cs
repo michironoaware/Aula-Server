39fb9cf82f94b01ece5db131629df72b6f3733cc
michironoaware <77251557+michironoaware@users.noreply.github.com>
2025-05-29 17:49:34 -0300

    Create RoleUpdatedEventDispatcher.cs

---
+++ Aula.Server/Features/Gateway/Events/RoleUpdated/RoleUpdatedEventDispatcher.cs
@@ -0,0 +1,28 @@
+﻿using Aula.Server.Domain.AccessControl;
+using Aula.Server.Shared.ApiContracts;
+using Aula.Server.Shared.Gateway;
+using MediatR;
+
+namespace Aula.Server.Features.Gateway.Events.RoleUpdated;
+
+internal sealed class RoleUpdatedEventDispatcher : INotificationHandler<RoleUpdatedEvent>
+{
+	private readonly GatewayManager _gatewayManager;
+
+	public RoleUpdatedEventDispatcher(GatewayManager gatewayManager)
+	{
+		_gatewayManager = gatewayManager;
+	}
+
+	public async Task Handle(RoleUpdatedEvent notification, CancellationToken cancellationToken)
+	{
+		var payload = new GatewayPayload<RoleData>
+		{
+			Operation = OperationType.Dispatch,
+			Event = EventType.RoleUpdated,
+			Data = notification.Role.ToRoleData(),
+		};
+
+		await _gatewayManager.DispatchEventAsync(payload);
+	}
+}
