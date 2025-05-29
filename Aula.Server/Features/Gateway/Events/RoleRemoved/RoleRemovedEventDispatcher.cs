4bc9635e9599b84257ba4cc5e5f4dfe4ef471d1b
michironoaware <77251557+michironoaware@users.noreply.github.com>
2025-05-29 17:49:30 -0300

    Create RoleRemovedEventDispatcher.cs

---
+++ Aula.Server/Features/Gateway/Events/RoleRemoved/RoleRemovedEventDispatcher.cs
@@ -0,0 +1,28 @@
+﻿using Aula.Server.Domain.AccessControl;
+using Aula.Server.Shared.ApiContracts;
+using Aula.Server.Shared.Gateway;
+using MediatR;
+
+namespace Aula.Server.Features.Gateway.Events.RoleRemoved;
+
+internal sealed class RoleRemovedEventDispatcher : INotificationHandler<RoleDeletedEvent>
+{
+	private readonly GatewayManager _gatewayManager;
+
+	public RoleRemovedEventDispatcher(GatewayManager gatewayManager)
+	{
+		_gatewayManager = gatewayManager;
+	}
+
+	public async Task Handle(RoleDeletedEvent notification, CancellationToken cancellationToken)
+	{
+		var payload = new GatewayPayload<RoleData>
+		{
+			Operation = OperationType.Dispatch,
+			Event = EventType.RoleDeleted,
+			Data = notification.Role.ToRoleData(),
+		};
+
+		await _gatewayManager.DispatchEventAsync(payload);
+	}
+}
