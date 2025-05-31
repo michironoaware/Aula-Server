e5921cfcc6fd4b0fb5371d5146c33be58c65ec50
michironoaware <77251557+michironoaware@users.noreply.github.com>
2025-05-31 17:03:37 -0300

    Create UserCommand.cs

---
+++ Aula.Server/Features/Commands/User/UserCommand.cs
@@ -0,0 +1,21 @@
+﻿using Aula.Server.Features.Commands.Help;
+using Aula.Server.Shared.Cli;
+
+namespace Aula.Server.Features.Commands.User;
+
+internal sealed class UserCommand : Command
+{
+	public UserCommand(IServiceProvider serviceProvider)
+		: base(serviceProvider)
+	{ }
+
+	internal override String Name => "user";
+
+	internal override String Description => "Shows a list of user related commands.";
+
+	internal override async ValueTask Callback(IReadOnlyDictionary<String, String> args, CancellationToken ct)
+	{
+		var helpCommand = ServiceProvider.GetRequiredService<HelpCommand>();
+		await helpCommand.Callback(Name, ct);
+	}
+}
