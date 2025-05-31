518e104b39fd7562d51ede8323e5c2458e3f98e8
michironoaware <77251557+michironoaware@users.noreply.github.com>
2025-05-31 17:03:32 -0300

    Create PermissionsSubCommand.cs

---
+++ Aula.Server/Features/Commands/Permissions/PermissionsSubCommand.cs
@@ -0,0 +1,23 @@
+﻿using Aula.Server.Features.Commands.Help;
+using Aula.Server.Shared.Cli;
+
+namespace Aula.Server.Features.Commands.Permissions;
+
+internal sealed class PermissionsSubCommand : Command
+{
+	public PermissionsSubCommand(IServiceProvider serviceProvider)
+		: base(serviceProvider)
+	{
+		AddSubCommand<ListPermissionsSubCommand>();
+	}
+
+	internal override String Name => "permissions";
+
+	internal override String Description => "Shows a list of user permissions related commands.";
+
+	internal override async ValueTask Callback(IReadOnlyDictionary<String, String> args, CancellationToken ct)
+	{
+		var helpCommand = ServiceProvider.GetRequiredService<HelpCommand>();
+		await helpCommand.Callback(Name, ct);
+	}
+}
