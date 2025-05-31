4a3c25a55dd3962f00fb99a76fb00dda8a38d832
michironoaware <77251557+michironoaware@users.noreply.github.com>
2025-05-31 18:10:29 -0300

    Create RoleCommand.cs

---
+++ Aula.Server/Features/Commands/Roles/RoleCommand.cs
@@ -0,0 +1,27 @@
+﻿using Aula.Server.Features.Commands.Help;
+using Aula.Server.Shared.Cli;
+
+namespace Aula.Server.Features.Commands.Roles;
+
+internal sealed class RoleCommand : Command
+{
+	public RoleCommand(IServiceProvider serviceProvider)
+		: base(serviceProvider)
+	{
+		AddSubCommand<CreateRoleSubCommand>();
+		AddSubCommand<SetRolePermissionsSubCommand>();
+		AddSubCommand<DeleteRoleSubCommand>();
+	}
+
+	internal override String Name => "role";
+
+	internal override String Description => "Shows a list of role related commands.";
+
+	internal override async ValueTask Callback(
+		IReadOnlyDictionary<String, String> args,
+		CancellationToken cancellationToken)
+	{
+		var helpCommand = ServiceProvider.GetRequiredService<HelpCommand>();
+		await helpCommand.Callback(Name, cancellationToken);
+	}
+}
